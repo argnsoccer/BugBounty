3a97e43773ab4a2f48ee0b0e835fdade85b6c599
drizzutojr <drizzuto@smu.edu>
2015-10-27 20:27:48 -0500

    moving towards better design

--- _app/_views/home.php
+++ _app/_views/home.php
@@ -1,54 +1,66 @@
-<?php
-session_destroy();
-session_start();
-?>
-
+  <?php
+  session_destroy();
+  session_start();
+  ?>
 
 <html>
-<head>
-    <title>I ain't afraid of no bugs!</title>
-    <link rel="shortcut icon" type="image/x-icon" href="../_images/_logos/bug-hunter-icon.ico" />
-    <link rel="stylesheet" type="text/css" href="../_css/home.css">
-  
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script type="text/javascript" src="_javascript/sign_up.js"></script>
+  <head>
+      <title>I ain't afraid of no bugs!</title>
+      <link rel="shortcut icon" type="image/x-icon" href="../_images/_logos/bug-hunter-icon.ico" />
+      <link rel="stylesheet" type="text/css" href="../_css/home.css">
 
-</head>
-<body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="_javascript/sign_up.js"></script>
+
+  </head>
+  <body>
 
 {{include ('header_login.php')}}
 
-    <div id="banner">
-        <span class="vert-align-helper"></span>
-        <img id="logo" src="../_images/_logos/logo_285x255.png" />
+    <div class="section1A" id="divSection1">
+      <div id="features">
+        <table>
+          <tr>
+            <td><img class="feature-icon" src="../_images/_icons/marshall-to-hunters.png"/></td>
+            <td class="feature-text">Connect to a large base of bug hunters.</td>
+          </tr>
+          <tr>
+            <td><img class="feature-icon" src="../_images/_icons/get-paid.png"></td>
+            <td class="feature-text">Get paid for finding bugs.</td>
+          </tr>
+        </table>
+      </div>
     </div>
-    <div id="content_area">
-        <div id="features">
-            <table>
-                <tr>
-                    <td><img class="feature-icon" src="../_images/_icons/marshall-to-hunters.png"/></td>
-                    <td class="feature-text">Connect to a large base of bug hunters.</td>
-                </tr>
-                <tr>
-                    <td><img class="feature-icon" src="../_images/_icons/get-paid.png"></td>
-                    <td class="feature-text">Get paid for finding bugs.</td>
-                </tr>
-            </table>
-        </div>
-          <form id="signUp" class="signUpForm" method="post">
-          <input class="signUpForm" type="text" placeholder="Your email" id="signUpEmail">
-          <input class="signUpForm" type="text" placeholder="Choose a username" id="signUpUsername">
-          <input class="signUpForm" type="password" placeholder="Choose a password" id="signUpPassword">
-          <span id="radio_buttons" class="signUp">
-            Hunter<input type="radio" name="accountType" value="hunter" id="signUpHunter" />
-            Marshall<input type="radio" name="accountType" value="marshall" id="signUpMarshall"/>
-          </span>
-          <input class="signUpForm" type="submit" value="Sign Up!" id="submitSignUp">
-        </form>
+
+    <div class="section2A" id="divSection2A">
+      <form id="signUp" class="signUpForm" method="post">
+        <input class="signUpForm" type="text" placeholder="Your email" id="signUpEmail">
+        <input class="signUpForm" type="text" placeholder="Choose a username" id="signUpUsername">
+        <input class="signUpForm" type="password" placeholder="Choose a password" id="signUpPassword">
+        
+        <span id="radio_buttons" class="signUp">
+          Hunter<input type="radio" name="accountType" value="hunter" id="signUpHunter" />
+          Marshall<input type="radio" name="accountType" value="marshall" id="signUpMarshall"/>
+        </span>
+        
+        <input class="signUpForm" type="submit" value="Sign Up!" id="submitSignUp">
+          </form>
+    </div>
+    
+    <div class="section1B" id="divSection1B">
+      <p>Wanted</p>
+    </div>
+    
+    <div class="section2B" id="divSection2B">
+      <p>Wanted</p>
+    </div>
+    
+    <div class="section3B" id="divSection3B">
+      <p>Wanted</p>
     </div>
 
 {{include('footer.php')}}
 
-</body>
+  </body>
 
 </html>
