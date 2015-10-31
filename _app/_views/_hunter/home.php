6ce86850fb9c6dce58a0913919d54fa182712960
Eric Hawkins <ehawkins@smu.edu>
2015-10-31 17:34:20 -0500

    Worked on hunter's home page.  It still needs a lot of work.  We should consider using Bootstrap.

--- _app/_views/_hunter/home.php
+++ _app/_views/_hunter/home.php
@@ -10,18 +10,20 @@
     <div id="content_area">
       <div id="display-boxes">
         <div class="display-box">
+          <a href="/_hunter/discover.php">Discover</a>
+          <p>Find hot young bugs in your local area</p>
         </div>
         <div class="display-box">
+          <a href="/_hunter/hunt.php">Hunt</a>
+          <p>Hunt the <i>hell</i> out of those bugs</p>
         </div>
-        <div class="display-box">
+        <div class="display-box" id="tricky_bugger">
+          <a href="/_hunter/search.php">Search</a>
+          <p>Has anyone really been far even as decided to use even go want to do look more like?  We'll find out this Sunday.</p>
         </div>
       </div>
-
+      <a id="prof_link" href="_hunter/profile/{{username}}">Link to Profile Page</a>
     </div>
-
+    
   </body>
 </html>
-
-
-
-<a href="_hunter/profile/{{username}}">Link to Profile Page</a>
