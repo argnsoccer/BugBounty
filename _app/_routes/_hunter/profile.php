9d3786d86f90f9e510b5be10a752cedebd2eefd3
Zachary <zfout@smu.edu>
2015-11-06 13:47:17 -0600

    updated profile rendering function to correctly access error codes

--- _app/_routes/_hunter/profile.php
+++ _app/_routes/_hunter/profile.php
@@ -8,7 +8,6 @@ function prepareHunterProfile($dbh, $username)
 	// 	3)  the path to the profile picture
 	// 	4)  all the reports this user has ever submitted
 	// 		(paid and non-paid) (all info including bountyID)
-		
 
 	$template_array = array();
 
@@ -24,37 +23,27 @@ function prepareHunterProfile($dbh, $username)
 
     if (isset($row['username'])) {
 			$template_array = array(
-
 				'error' => array('code' => '0', 'message' => 'success'),
 				'user' => array("username" => $row['username'], "email" => $row['email'], "userID" => $row['userID'])
 			);
-
     }
     else {
     	$template_array['error'] = array(
     		"code" => '1',
     		"message" => 'No username'
 			);
-
     	return $template_array;
     }
-
 	}
 	else {
 		$template_array = array(
     		"error" => '2',
-    		"message" => 'Statement not ran'
+    		"message" => 'Statement was not executed successfully'
     	);
-
     	return $template_array;
 	}
-
 	return $template_array;
 }
-
-
-
-
 
 $app->get('/_hunter/profile', function() use ($app) {
 	echo "include a username";
@@ -71,15 +60,15 @@ $app->get('/_hunter/profile/:username', function($username) use ($app, $dbh) {
 
 			$template_array = prepareHunterProfile($dbh, $username);
 
-			if($template_array['error'] == 0)
+			if($template_array['error']['code'] == 0)
 			{
 				$app->render('_hunter/profile.php', $template_array);
 			}
-			else if ($template_array['error'] == 1)
+			else if ($template_array['error']['code'] == 1)
 			{
 				echo "error - No username is database with passed username";
 			}
-			else if ($template_array['error'] == 1)
+			else if ($template_array['error']['code'] == 1)
 			{
 				echo "error - statement could not be run";
 			}
