1994dedd0116f824181865a912fd7b5ad64b9122
Eric Hawkins <ehawkins@smu.edu>
2015-11-27 21:53:21 -0600

    Added some styling to contact page, but it still looks too bare. I'm not sure what else to add. Added margin-bottom to content area on about page.

--- _app/_views/contact.php
+++ _app/_views/contact.php
@@ -7,6 +7,7 @@
 <head>
     <title>"Who ya gonna call?"</title>
     <link rel="shortcut icon" type="image/x-icon" href="../_images/_logos/bug-hunter-icon.ico" />
+    <link rel="stylesheet" type="text/css" href="/../_css/contact.css" />
     <link rel="stylesheet" type="text/css" href="/../_css/header.css">
     <link rel="stylesheet" type="text/css" href="/../_css/default.css">
 <!--     <link rel="stylesheet" type="text/css" href="/../_css/contact.css"> -->
@@ -15,7 +16,7 @@
 </head>
 
 <body>
-    
+
 {% if username and userType == "hunter" %}
   {{ include ('header_hunter.php') }}
 {% elseif  username and userType == "marshall" %}
@@ -23,36 +24,39 @@
 {% else %}
   {{ include ('header_login.php') }}
 {% endif %}
-    
-    <h1 id="main_header">Contact BugBounty</h1>
-    <hr />
 
-    <div class="contact-block">
-        <h3>General Information</h3>
-        <p>sales@bugbounty.com</p>
+    <div id=content_area class="marshalBackground">
+        <h1 id="main_header">Contact BugBounty</h1>
+        <hr />
+
+        <div class="contact-block">
+            <h3>General Information</h3>
+            <p>sales@bugbounty.com</p>
+        </div>
+
+        <div class="contact-block">
+            <h3>Dallas Office</h3>
+            <p>3145 Dyer Street</p>
+            <p>Dallas, TX 75275</p>
+            <p>Tel: (985) 655-2500</p>
+            <p>Fax: (221) 867-5309</p>
+        </div>
+
+        <div class="contact-block">
+            <h3>New York Office</h3>
+            <p>12 North Moore Street</p>
+            <p>New York, NY 10013</p>
+            <p>Tel: (718) 777-3456</p>
+            <p>Fax: (348) 555-2368</p>
+        </div>
     </div>
 
-    <div class="contact-block">
-        <h3>Dallas Office</h3>
-        <p>3145 Dyer Street</p>
-        <p>Dallas, TX 75275</p>
-        <p>Tel: (985) 655-2500</p>
-        <p>Fax: (221) 867-5309</p>
-    </div>
-
-    <div class="contact-block">
-        <h3>New York Office</h3>
-        <p>12 North Moore Street</p>
-        <p>New York, NY 10013</p>
-        <p>Tel: (718) 777-3456</p>
-        <p>Fax: (348) 555-2368</p>
-    </div>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script type="text/javascript" src="/../_javascript/basic_search.js"></script>
   <script type="text/javascript" src="/../_javascript/login.js"></script>
   <script type="text/javascript" src="/../_javascript/logout.js"></script>
-  {{include ('bootstrap_footer.php')}} 
+  {{include ('bootstrap_footer.php')}}
 
 </body>
 </html>
