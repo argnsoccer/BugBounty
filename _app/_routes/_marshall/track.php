38f16c8ab647c10d665f64c4518fd073d217ee26
Eric Hawkins <ehawkins@smu.edu>
2015-12-09 12:02:29 -0600

    Styled Track page

--- _app/_routes/_marshall/track.php
+++ _app/_routes/_marshall/track.php
@@ -3,11 +3,51 @@
 session_start();
 session_set_cookie_params(0);
 
+function prepareTrackPage($dbh, $company, $bountyID)
+{
+	$template_array['company'] = $company;
+	$args[':bountyID'] = $bountyID;
+	$template_array['bounty'] = getBountyFromBountyID($dbh, $args);
+	//$template_array['reports'] = getReportsFromBountyID($dbh, $args);
+
+	//Test reports
+	$template_array['reports'] = array(
+		array(
+			'dateCreated' => '2015-10-29',
+			'name' => 'Tupac the Eskimo'
+		),
+		array(
+			'dateCreated' => '2015-10-29',
+			'name' => 'Tupac the Eskimo'
+		),
+		array(
+			'dateCreated' => '2015-10-29',
+			'name' => 'Tupac the Eskimo'
+		),
+		array(
+			'dateCreated' => '2015-10-29',
+			'name' => 'Tupac the Eskimo'
+		),
+		array(
+			'dateCreated' => '2015-10-29',
+			'name' => 'Tupac the Eskimo'
+		)
+	);
+
+	$template_array['numReports'] = sizeof($template_array['reports']);
+
+	return $template_array;
+}
+
 $app->get('/_marshall/track', function() use ($app) {
 	echo "please include bounty id";
 });
 
-$app->get('/_marshall/track/:bountyID', function($bountyID) use ($app) {
-	echo $bountyID;
-	$app->render('_marshall/track.php');
+$app->get('/_marshall/track/:company/:bountyID', function($company, $bountyID) use ($app, $dbh)
+{
+	$template_array = prepareTrackPage($dbh, $company, $bountyID);
+
+	$template_array['test'] =  "hey";
+	$app->render('_marshall/track.php', $template_array);
+	echo print_r($template_array);
 });
--- _app/_views/_marshall/track.php
+++ _app/_views/_marshall/track.php
@@ -16,50 +16,101 @@
   </head>
   <body>
     {{ include ('header_marshall.php') }}
-    <div class="container">
+    <div id="content_area">
         <div class="row">
             <div class="col-sm-6">
-                <h3>Bounty Information</h3>
-                <button type="button" class="btn btn-default btn-xs pull-right" data-toggle="modal"
-                  data-target="#profileChangeModal" data-whatever="@getbootstrap">
-                        Edit
-                </button>
-                <form id="profileUpdateForm" method="post">
-                  <div class="form-group">
-                      <label for="InputName"></label>
-                      <div class="input-group">
-                        <span class="input-group-addon addOnCustom">Bounty Name</span>
-                        <p class="form-control formControlCustom">{{user.username}}</p>
-                      </div>
-                  </div>
-                  <div class="form-group">
-                      <label for="InputName"></label>
-                      <div class="input-group">
-                        <span class="input-group-addon addOnCustom">Date Ending</span>
-                        <p class="form-control formControlCustom">{{user.name}}</p>
-                      </div>
-                  </div>
-                  <div class="form-group">
-                      <label for="InputName"></label>
-                      <div class="input-group">
-                        <span class="input-group-addon addOnCustom">Date Created</span>
-                        <p class="form-control formControlCustom">{{user.email}}</p>
-                      </div>
-                  </div>
-                  <div class="form-group">
-                      <label for="InputName"></label>
-                      <div class="input-group">
-                        <span class="input-group-addon addOnCustom">Reports</span>
-                        <p class="form-control formControlCustom">{{user.dateJoined}}</p>
-                      </div>
-                  </div>
-                </form>
-                <div id="info_box">
+                <div class="row innerRow">
+                    <div class="col-sm-4">
+                        <input type="image" id="bounty_picture" class="marshalBackground" src="{{bounty.imageLoc}}" alt="Bounty Picture" />
+                    </div>
+                    <div class="col-sm-8">
+                        <h3>Bounty Information</h3>
+                        <button type="button" class="btn btn-default btn-xs pull-right" data-toggle="modal"
+                          data-target="#profileChangeModal" data-whatever="@getbootstrap">
+                                Edit
+                        </button>
+                        <form id="profileUpdateForm" method="post">
+                          <div class="form-group">
+                              <label for="InputName"></label>
+                              <div class="input-group">
+                                <span class="input-group-addon addOnCustom">Bounty Name</span>
+                                <p class="form-control formControlCustom">{{bounty.bountyName}}</p>
+                              </div>
+                          </div>
+                          <div class="form-group">
+                              <label for="InputName"></label>
+                              <div class="input-group">
+                                <span class="input-group-addon addOnCustom">Date Ending</span>
+                                <p class="form-control formControlCustom">{{bounty.dateEnding}}</p>
+                              </div>
+                          </div>
+                          <div class="form-group">
+                              <label for="InputName"></label>
+                              <div class="input-group">
+                                <span class="input-group-addon addOnCustom">Date Created</span>
+                                <p class="form-control formControlCustom">{{bounty.dateCreated}}</p>
+                              </div>
+                          </div>
+                          <div class="form-group">
+                              <label for="InputName"></label>
+                              <div class="input-group">
+                                <span class="input-group-addon addOnCustom">Reports</span>
+                                <p class="form-control formControlCustom">{{numReports}}</p>
+                              </div>
+                          </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="row innerRow">
+                    <div id="info_box">
 
+                    </div>
                 </div>
             </div>
             <div class="col-sm-6">
-
+                <h3 class="table-title">Submitted Reports</h3>
+                <div class="tableWrapper">
+                  <table>
+                    <thead>
+                      <tr class="rowTable header">
+                        <th class="cell">Date Submitted</th>
+                        <th class="cell">Report Name</th>
+                        <th class="cell">Details</th>
+                        <th class="cell">Pay</th>
+                        <th class="cell">       </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {% for report in reports %}
+                      <tr class="table-row">
+                        <td class="cell">{{report.dateCreated}}</td>
+                        <td class="cell">{{report.name}}</td>
+                        <td class="cell">
+                          <button type="button" class="detailsButton" data-toggle="modal"
+                          data-target="#detailsModal" data-whatever="@getbootstrap"
+                          data-ID={{report.reportID}}>
+                            View
+                          </button>
+                        </td>
+                        <td class="cell">
+                            <span id="pay_amount" class="input-group">
+                                <span id="dollar_sign" class="input-group-addon">$</span>
+                                <input id="pay_amount_field" class="form-control input-sm" type="text" name="payAmount" maxlength="7" autocomplete="off" />
+                            </span>
+                          <!--<button type="button" class="messageButton" data-toggle="modal"
+                            data-target="#messageModal" data-whatever="@getbootstrap"
+                            data-ID={{report.reportID}}>
+                            View
+                        </button>-->
+                        </td>
+                        <td class="cell">
+                            <input class="btn btn-success" type="submit" value="Pay" name="submit" />
+                        </td>
+                      </tr>
+                      {% endfor %}
+                    </tbody>
+                  </table>
+                </div>
             </div>
         </div>
     </div>
