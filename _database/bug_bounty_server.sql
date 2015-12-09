5fc594d2232be5821f3a3669fde57bc25fa5cfb1
mgilbert <mgilbert@smu.edu>
2015-12-09 17:18:58 -0600

    db change

--- _database/bug_bounty_server.sql
+++ _database/bug_bounty_server.sql
@@ -304,7 +304,7 @@ CREATE TABLE IF NOT EXISTS `MessageOfDay` (
   `messageID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `accountType` varchar(10) NOT NULL,
   `message` mediumtext NOT NULL,
-  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `dateMade` datetime DEFAULT NOW(),
   PRIMARY KEY (`messageID`),
   UNIQUE KEY `messageID_UNIQUE` (`messageID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
