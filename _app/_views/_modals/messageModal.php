6084314e8fd0f1f7c0698e691ac860a78182d2d2
drizzutojr <drizzuto@smu.edu>
2015-12-10 17:39:17 -0600

    cleaned up, fine tuned modals

---
+++ _app/_views/_modals/messageModal.php
@@ -0,0 +1,17 @@
+      <div class="modal fade" id="messageModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="exampleModalLabel">Message</h4>
+               <div class="modal-body">
+                <div class="form-group">
+                  <label for="recipient-name" class="control-label">Message:</label>
+                  <textarea id="messageReport" readonly>
+                  </textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
---
+++ _app/_views/_modals/paymentModal.php
@@ -0,0 +1,25 @@
+      <div class="modal fade" id="paymentChangeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="exampleModalLabel">Change Payment Details</h4>
+              <form id="reportForm" data-ID={{bounty.id}} data-username={{username}}>
+                <div class="modal-body">
+                  <div class="input-group input-group-Modal">
+                    <span class="input-group-addon addOnCustom">Option 1</span>
+                    <input type ="text" class="form-control modalInput" id="changePaymentOption1Form">
+                  </div>
+                  <div class="input-group input-group-Modal">
+                    <span class="input-group-addon addOnCustom">Password</span>
+                    <input type ="password" class="form-control modalInput" id="changePaymentPasswordForm">
+                  </div>
+                </div>
+              </form>
+              <div class="modal-footer">
+                <button type="submit" class="btn btn-primary" id="submitChangePayment">Submit</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
