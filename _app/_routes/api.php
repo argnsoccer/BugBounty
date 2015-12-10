c71e4953cb80dab95c4cc1264590c5546d6eb810
Andre Gras <argnsoccer@gmail.com>
2015-12-09 19:59:41 -0600

    recent reports API wit pythons and reptiles and tortulas and bearded lizard and sheeeeeeeeeeeeeeeeeeee

--- _app/_routes/api.php
+++ _app/_routes/api.php
@@ -781,6 +781,47 @@ function getBountyFromBountyID($dbh, $args) {
   return $functionArray;
 }
 
+function getBountiesFromUsernameRecentReports($dbh,$args)
+{
+  $functionArray = array();
+  $statement = $dbh->prepare(
+  "SELECT BountyPool.* FROM BountyPool, Report WHERE BountyPool.poolID=Report.bountyID AND Report.username = :username
+  ORDER BY Report.dateSubmitted DESC LIMIT 4"
+);
+
+  if($statement->execute($args))
+  {
+    $functionArray['result'] = array();
+    while($row = $statement->fetch(PDO::FETCH_ASSOC))
+    {
+      $insert = true;
+      foreach ($functionArray['result'] as $iterator)
+      {
+        if($row['poolID'] == $iterator['poolID'])
+        {
+          $insert = false;
+        }
+      }
+      if($insert)
+      {
+        $row['dateCreated'] = substr($row['dateCreated'], 0, -9);
+        $row['dateEnding'] = substr($row['dateEnding'], 0, -9);
+        array_push($functionArray['result'], $row);
+      }
+    }
+    $functionArray['error'] = '0';
+    $functionArray['message'] = 'success';
+
+
+  }
+  else {
+    $functionArray['error'] = '1';
+    $functionArray['message'] = 'Statement does not execute';
+    $functionArray['messageDB'] = $statement->errorInfo();
+  }
+  return $functionArray;
+}
+
 function createRSS($dbh, $args) {
 
   $file_path = $args['link']."/rss_".$args['username'].".xml";
@@ -1140,6 +1181,7 @@ $app->get('/api/emailTaken/:email', function($email) use ($dbh) {
   echo json_encode($functionArray);
 
 });
+
 
 /*
 Danny Rizzuto
@@ -1650,4 +1692,11 @@ $app->get('/api/getMODMarshall',function() use($dbh)
 	$args = array();
 	$args[':accountType'] = "Marshall";
 	echo json_encode(getMessageOfDay($dbh,$args));
+});
+
+$app->get('/api/getBountiesFromUsernameRecentReports/:username', function($username) use($dbh)
+{
+  $args[':username'] = $username;
+  echo json_encode(getBountiesFromUsernameRecentReports($dbh,$args));
+
 });
