a02bf30baac5ddb0d723d5fec8a403675404e0bf
drizzutojr <drizzuto@smu.edu>
2015-12-12 17:44:31 -0600

    rss stinks

--- _app/_routes/api.php
+++ _app/_routes/api.php
@@ -1098,6 +1098,10 @@ function getReportsFromMarshal($dbh,$args)
 
 function createRSS($dbh, $args) {
 
+
+  $dirs = array_filter(glob('*'), 'is_dir');
+  $function_array['fffff'] = $dirs;
+  
   $file_path = $args['link']."/rss_".$args['username'].".xml";
 
   $xml = "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n";
