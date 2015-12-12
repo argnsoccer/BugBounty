6d31e8e8c82016b0822a05fdfbbfe4544f29b03e
drizzutojr <drizzuto@smu.edu>
2015-12-12 17:53:37 -0600

    fdsa

--- _app/_routes/api.php
+++ _app/_routes/api.php
@@ -1102,8 +1102,6 @@ function createRSS($dbh, $args) {
   $dirs = array_filter(glob('*'), 'is_dir');
   $result['fffff'] = $dirs;
 
-  $result['heyhey'] = scandir("/");
-
   $file_path = $args['link']."/rss_".$args['username'].".xml";
 
   $xml = "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n";
