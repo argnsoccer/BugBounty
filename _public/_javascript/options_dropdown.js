ec38c6969181dec302eae144d75771d86a35ff13
drizzutojr <drizzuto@smu.edu>
2015-12-11 18:25:49 -0600

    working on opitons

--- _public/_javascript/options_dropdown.js
+++ _public/_javascript/options_dropdown.js
@@ -25,13 +25,43 @@ $(function() {
 	var accordion = new Accordion($('#accordion'), false);
 });
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36251023-1']);
-  _gaq.push(['_setDomainName', 'jqueryscript.net']);
-  _gaq.push(['_trackPageview']);
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function updateTable(optionsInfo) {
+	// console.log(optionsInfo);
+
+	var rows = $("tbody#bodyReports > tr");
+
+	for (row in rows) {
+		console.log(row);
+	}
+
+	// console.log(rows);
+
+}
+
+$(document).ready(function () {
+
+	$("#optionsSubmitForm").click(function(event) { 
+
+		var optionsInfo = [];
+
+		event.preventDefault();
+
+		$('input:checked').each(function() {
+			optionsInfo.push($(this).attr("value") + this.id);
+		});
+
+		updateTable(optionsInfo);
+
+	});
+});
